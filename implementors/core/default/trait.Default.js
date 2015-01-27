5d729d29bf7c8a4e99b419a1e197f7d97ea2acda
Steven Allen <steven@stebalien.com>
2015-01-26 22:35:24 -0500

    Update docs for 34c43bdc5062dcf6cc4c9f1583ee1228bbde5c5b
    impl Default

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['snowflake'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='snowflake/struct.Snowflake.html' title='snowflake::Snowflake'>Snowflake</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
