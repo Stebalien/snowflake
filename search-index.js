70c6df3c71f58125f544fb308513948710008f71
Steven Allen <steven@stebalien.com>
2015-01-26 22:35:12 -0500

    Update docs for b2215f0ca94422970478242ed613b468f45e81b9
    Put snowflake constructor in snowflake impl.

--- search-index.js
+++ search-index.js
@@ -1,3 +1,3 @@
 var searchIndex = {};
-searchIndex['snowflake'] = {"items":[[0,"","snowflake","A module for quickly generating IDs guarenteed to be unique within the process, for the\nlifetime of the process."],[3,"Snowflake","","An opaque unique id."],[5,"new","","Create a new unique ID."],[11,"eq","","",0],[11,"ne","","",0]],"paths":[[3,"Snowflake"]]};
+searchIndex['snowflake'] = {"items":[[0,"","snowflake","A module for quickly generating IDs guarenteed to be unique within the process, for the\nlifetime of the process."],[3,"Snowflake","","An opaque unique id."],[11,"eq","","",0],[11,"ne","","",0],[11,"new","","Create a new unique ID.",0]],"paths":[[3,"Snowflake"]]};
 initSearch(searchIndex);
