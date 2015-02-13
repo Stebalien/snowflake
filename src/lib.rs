11156f106d1e0e907cdd8e4139979f98ce93aa20
Steven Allen <steven@stebalien.com>
2015-02-12 21:33:14 -0500

    Remove core feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(test, feature(core, test, std_misc))]
+#![cfg_attr(test, feature(test, std_misc))]
 //! A crate for quickly generating unique IDs with guaranteed properties.
 //!
 //! This crate currently includes guaranteed process unique IDs but may include new ID types in the
