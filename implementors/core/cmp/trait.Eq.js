527e44b8a6cc320c824d949863d4daf67f23d1d6
Steven Allen <steven@stebalien.com>
2015-05-11 08:51:15 -0400

    Update docs for ded337a9d30a80b5c232b02d7d24eccfa02f6ad4
    Add travis build status.

--- implementors/core/cmp/trait.Eq.js
+++ implementors/core/cmp/trait.Eq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['snowflake'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='struct' href='snowflake/struct.ProcessUniqueId.html' title='snowflake::ProcessUniqueId'>ProcessUniqueId</a>",];
+implementors['snowflake'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='struct' href='snowflake/struct.ProcessUniqueId.html' title='snowflake::ProcessUniqueId'>ProcessUniqueId</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/core/default/trait.Default.js
+++ implementors/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['snowflake'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='snowflake/struct.ProcessUniqueId.html' title='snowflake::ProcessUniqueId'>ProcessUniqueId</a>",];
+implementors['snowflake'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='snowflake/struct.ProcessUniqueId.html' title='snowflake::ProcessUniqueId'>ProcessUniqueId</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
