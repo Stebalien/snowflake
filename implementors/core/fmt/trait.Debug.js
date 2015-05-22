0686e865525bea2b164ed802ff28301a739f8a5e
Steven Allen <steven@stebalien.com>
2015-05-21 22:16:13 -0400

    Update docs for a90fca1319708af09caca1632c6135887407fa9f
    Ever so slightly faster...
    
    1.16x speedup.

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['snowflake'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='snowflake/struct.ProcessUniqueId.html' title='snowflake::ProcessUniqueId'>ProcessUniqueId</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
