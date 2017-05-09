018754159bab126cfe387ed19e9136373e9542fa
Steven Allen <steven@stebalien.com>
2017-05-08 17:11:43 -0700

    Update docs for e55768c0931d3015c322e6bed6d838806d78db53
    Release 1.3.0

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["serde"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"serde/de/value/struct.Error.html\" title=\"struct serde::de::value::Error\">Error</a>",];
+implementors["syn"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
